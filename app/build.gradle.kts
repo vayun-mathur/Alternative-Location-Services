49bb69b1037de01416ad84c72dbd34f07974fb04
Simó Albert i Beltran <sim6@probeta.net>
2025-02-18 11:03:49 +0100

    allow SDK 34
    Signed-off-by: Simó Albert i Beltran <sim6@probeta.net>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.opengps.altlocationservices"
-        minSdk = 35
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
